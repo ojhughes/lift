4f453829f580c8f0a2235c6e1591b1e13a857850
Ollie Hughes <olli.hughes@gmail.com>
2019-08-19 13:01:20 +0100

    Initial skeleton of lift built on dotnet core 3
    that publishes a single native binary for linux, macos and windows

---
+++ lift-dotnet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using ConsoleTables;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace lift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            app.Name = "lift";
+            app.OnExecute(() =>
+            {
+                Console.WriteLine("cloudlift.sh is a tool for enriching your application so it can be deployed to " +
+                                  "multiple cloud platforms with minimal effort.");
+                return 0;
+            });
+            app.Command("platform", (platformCommand) =>
+            {
+                platformCommand.Command("list", (platformListCommand) =>
+                {
+                    var table = new ConsoleTable("Name", "Alias", "Type", "Profile", "Description");
+                    table.AddRow("gke_cf-sandbox-ohughes", "ci-build", "kubernetes", "qa", "Google Kubernetes Engine")
+                        .AddRow("api.run.pivotal.io - FrameworksAndRuntimes/ohughes", "pws-ohughes", "cloudfoundry",
+                            "prod", "Cloud Foundry version 2.138.0");
+
+                    platformListCommand.OnExecute(() =>
+                    {
+                        table.Write(Format.Alternative);
+                        return 0;
+                    });
+                });
+            });
+            app.Execute(args);
+        }
+    }
+}
